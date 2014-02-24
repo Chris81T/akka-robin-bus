3327bbb7205bf6df65334f1828945a09427a65cf
Christian Thomas <christian@chr-thomas.de>
2014-02-24 19:42:37 +0100

    no idea to solve runtime akka error

--- src/main/scala/de/examples/Main.scala
+++ src/main/scala/de/examples/Main.scala
@@ -2,6 +2,7 @@ package de.examples
 
 import akka.actor.{Props, Actor, ActorSystem}
 import akka.routing.RoundRobinPool
+import scala.reflect.ClassTag
 
 /**
  * Created by christian on 2/24/14.
@@ -43,9 +44,37 @@ class Supervisor extends Actor {
   
 }
 
-class RoundRobinEventBusSubscriber[T <: Actor] extends Actor {
+/**
+ *
+ * GIVEN RUNTIME ERROR
+
+[akka-robin-bus-akka.actor.default-dispatcher-3] [akka://akka-robin-bus/user/$a] no matching constructor found on class de.examples.RoundRobinEventBusSubscriber for arguments []
+java.lang.IllegalArgumentException: no matching constructor found on class de.examples.RoundRobinEventBusSubscriber for arguments []
+	at akka.util.Reflect$.error$1(Reflect.scala:82)
+	at akka.util.Reflect$.findConstructor(Reflect.scala:106)
+	at akka.actor.NoArgsReflectConstructor.<init>(Props.scala:340)
+	at akka.actor.IndirectActorProducer$.apply(Props.scala:289)
+	at akka.actor.Props.producer(Props.scala:157)
+	at akka.actor.Props.<init>(Props.scala:170)
+	at akka.actor.Props$.apply(Props.scala:69)
+	at de.examples.Supervisor$$anonfun$receive$1.applyOrElse(Main.scala:33)
+	at akka.actor.Actor$class.aroundReceive(Actor.scala:465)
+	at de.examples.Supervisor.aroundReceive(Main.scala:27)
+	at akka.actor.ActorCell.receiveMessage(ActorCell.scala:491)
+	at akka.actor.ActorCell.invoke(ActorCell.scala:462)
+	at akka.dispatch.Mailbox.processMailbox(Mailbox.scala:238)
+	at akka.dispatch.Mailbox.run(Mailbox.scala:220)
+	at akka.dispatch.ForkJoinExecutorConfigurator$AkkaForkJoinTask.exec(AbstractDispatcher.scala:393)
+	at scala.concurrent.forkjoin.ForkJoinTask.doExec(ForkJoinTask.java:260)
+	at scala.concurrent.forkjoin.ForkJoinPool$WorkQueue.runTask(ForkJoinPool.java:1339)
+	at scala.concurrent.forkjoin.ForkJoinPool.runWorker(ForkJoinPool.java:1979)
+	at scala.concurrent.forkjoin.ForkJoinWorkerThread.run(ForkJoinWorkerThread.java:107)
+
+ *
+ */
+class RoundRobinEventBusSubscriber[T <: Actor: ClassTag] extends Actor { // <-------------------------------------------
   
-  private val routees = context.actorOf(RoundRobinPool(5).props(Props[StrSubscriber]))
+  private val routees = context.actorOf(RoundRobinPool(5).props(Props[T])) // <-----------------------------------------
 
   override def preStart {
     super.preStart
